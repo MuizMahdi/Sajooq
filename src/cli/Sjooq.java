2af21625619390e33933bb41d060211fe6ebab13
muizz <muizzmahdy@gmail.com>
2021-08-07 15:48:47 +0400

    Add DB credentials options to CLI

--- src/cli/Sjooq.java
+++ src/cli/Sjooq.java
@@ -11,15 +11,15 @@ import java.util.concurrent.Callable;
 
 
 @Command(
-    name = "sjooq", 
+    name = "sajooq", 
     mixinStandardHelpOptions = true, 
-    version = "SJOOQ Cli 0.1",
-    description = "A CLI with a set of utilities for managing Spring applications using Hibernate, Liquibase, and JOOQ")
+    version = "SAJOOQ Cli 0.1",
+    description = "A CLI with a set of commands for managing entities lifecycle using Hibernate, Liquibase, and JOOQ")
 class Sjooq implements Callable<Integer> {
 
     @Parameters(
         index = "0",
-        description = "Changelog, Entity, or Database which Sjooq will operate on" 
+        description = "Changelog, Entity, or Database which Sajooq will operate on" 
     )
     private String operand;
 
@@ -47,6 +47,34 @@ class Sjooq implements Callable<Integer> {
     private String entity;
 
 
+    @Option(
+        names = {"-n", "--db-name"},
+        description = "Database name",
+        required = true
+    )
+    private String dbName;
+
+
+    @Option(
+        names = {"-u", "--db-user"},
+        description = "Database user",
+        required = true
+    )
+    private String dbUser;
+
+
+    @Option(
+        names = {"-p", "--db-password"},
+        description = "Database user's password",
+        required = true
+    )
+    private String dbPassword;
+
+
+    /** 
+     * Initializes CLI
+     * @param args
+     */
     public static void main(String... args) {
         int exitCode = new CommandLine(new Sjooq()).execute(args);
         System.exit(exitCode);
@@ -56,6 +84,11 @@ class Sjooq implements Callable<Integer> {
     private boolean isChangelog, isEntity, isDatabase, isAdding, isUpdating;
 
 
+    /** 
+     * Runs CLI command
+     * @return Integer
+     * @throws Exception
+     */
     @Override
     public Integer call() throws Exception {
 
@@ -83,7 +116,14 @@ class Sjooq implements Callable<Integer> {
     }
 
 
+    /** 
+     * Checks if all CLI operands and their operations are valid for execution
+     * @return boolean
+     */
     private boolean areProcessesValid() {
+
+        // Validate database credentials
+        if (!areDbCredentialsValid()) return false;
 
         // Validate operands
         if ((!isChangelog) && (!isEntity) && (!isDatabase)) {
@@ -120,6 +160,28 @@ class Sjooq implements Callable<Integer> {
     }
 
 
+    /**
+     * Checks if Database CLI options are entered and valid for execution
+     * @return
+     */
+    private boolean areDbCredentialsValid() {
+
+        var isDbNameValid = (dbName != null && !dbName.trim().isEmpty());
+        var isDbUserValid = (dbUser != null && !dbUser.trim().isEmpty());
+        var isDbPassValid = (dbPassword != null && !dbPassword.trim().isEmpty());
+
+        if ((!isDbNameValid) && (!isDbUserValid) && (!isDbPassValid)) {
+            printlnAnsi("@|red Invalid database credentials, please enter database name, user, and password|@");
+            return false;
+        }
+
+    }
+
+
+    /** 
+     * Runs a process according to the entered operands and operations
+     * @throws Exception
+     */
     private void runProccesses() throws Exception {
 
         // Check for changelog generation from database
@@ -134,12 +196,12 @@ class Sjooq implements Callable<Integer> {
 
         // Check for entity generation/update
         if (isEntity && changelog != null && !changelog.isEmpty()) {
-            // TODO: --
+            generateEntityfromChangelog();
         }
 
         // Check for database update
         if (isDatabase && changelog != null && !changelog.isEmpty()) {
-            // TODO: --
+            updateDatabaseFromChangelog();
         }
 
         RunCommand("mvn clean install");
@@ -147,16 +209,69 @@ class Sjooq implements Callable<Integer> {
     }
 
 
-    private void generateChangelogFromDb() {
-        
+    /**
+     * Generates changelog from main database
+     * @throws Exception
+     */
+    private void generateChangelogFromDb() throws Exception {
+
+        var command = String.format(
+            "mvn liquibase:generateChangeLog "
+            + "-DDB_NAME=%n "
+            + "-DDB_USER=%u "
+            + "-DDB_PASSWORD=%p", 
+            dbName, dbUser, dbPassword
+        );
+
+        RunCommand(command);
     }
 
-    
-    private void generateChangelogFromEntity() {
 
+    /**
+     * Generates and updates entities from changelog
+     * @throws Exception
+     */
+    private void generateEntityfromChangelog() throws Exception {
+
+        var command = String.format(
+            "mvn org.jooq:jooq-codegen-maven:3.15.1:generate@generate-entity "
+            + "-DDB_NAME=%n "
+            + "-DDB_USER=%u "
+            + "-DDB_PASSWORD=%p", 
+            dbName, dbUser, dbPassword
+        );
+
+        RunCommand(command);
     }
 
 
+    /**
+     * Generates changelog from JPA entities, this is done
+     * using JOOQ's JPADatabase code generator, which:
+     * 1. Uses Spring to find all JPA annotated entities in classpath,
+     * 2. Creates DB tables for these entities in a local H2 DB,
+     * 3. Generates the JOOQ metamodel classes from the difference
+     * between the local H2 DB and the production DB.
+     * @throws Exception
+     */
+    private void generateChangelogFromEntity() throws Exception {
+        RunCommand("");
+    }
+
+
+    /**
+     * Updates the main DB using liquibase changelogs
+     * @throws Exception
+     */
+    private void updateDatabaseFromChangelog() throws Exception {
+        RunCommand("");
+    }
+
+
+    /** 
+     * @param command
+     * @throws Exception
+     */
     private void RunCommand(String command) throws Exception {
 
         boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
@@ -176,7 +291,10 @@ class Sjooq implements Callable<Integer> {
 
     }
 
-
+    
+    /** 
+     * @param msg
+     */
     private void printlnAnsi(String msg) {
         System.out.println(CommandLine.Help.Ansi.AUTO.string(msg));
     }
